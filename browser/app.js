aa93719d4817936547e991253380d62343cf0013
zhengshizhao <zhao.zhengshi@gmail.com>
2016-11-20 02:52:54 -0500

    add search section

--- browser/app.js
+++ browser/app.js
@@ -1,5 +1,5 @@
 'use strict';
-window.app = angular.module('MetasearchAPP', ['ngRoute','ui.router']);
+window.app = angular.module('MetasearchAPP', ['ui.router']);
 
 app.config(function ($urlRouterProvider, $locationProvider) {
     // This turns off hashbang urls (/#about) and changes it to something normal (/about)
---
+++ browser/js/search/search.js
@@ -0,0 +1,79 @@
+app.directive('keywordSearch', function ($state) {
+    return {
+        restrict: 'E',
+        templateUrl: 'js/search/search.html',
+        controller: 'searchCrl'
+    };
+});
+app.service('searchService', function(){
+	var cusine = "";
+	var addcusine = function(_cusine){
+		cusine = _cusine;
+	}
+	var getcusine = function(){
+		if(cusine.length>=1) return cusine;
+		else return "";
+	}
+	return {
+		addcusine: addcusine,
+		getcusine: getcusine
+	};
+});
+app.controller('searchCrl', function($scope,$window,$state,searchService){
+
+	var recognition = new $window.webkitSpeechRecognition();
+	recognition.continuous = true;
+	//recognition.interimResults = true;
+	
+	var text = $window.document.getElementById('textinput');
+	recognition.onend = function(event){
+		if(event) $scope.reset();
+	};
+    
+	$scope.submit = function(event){
+		console.log("evet",event);
+		if(event.keyCode === 13) {
+			recognition.stop();
+			$state.go("recipes",{cusine:text.value});
+		}
+	}
+	recognition.onresult = function (event) {
+		
+			console.log("text", text.value);
+          for (var i = event.resultIndex; i < event.results.length; ++i) {
+            // if (!event.results.final) {
+            // 	text.value += event.results[i][0].transcript;
+            //     console.log("text value:", text.value);
+            // }
+            if (!event.results.final) {
+            	text.value = event.results[i][0].transcript;
+                console.log("text value:", text.value);
+            }
+
+          }
+        $scope.keyword = text.value;
+        searchService.addcusine($scope.keyword);
+         // $scope.keyword = event.results[0][0].transcript;
+        // $scope.confidence =  event.results[0][0].confidence;
+    };
+    $scope.reset = function () {
+        $scope.isSpeaking = false; 
+    }
+	$scope.isSpeaking = false;
+	$scope.toggleStartStop = function(){
+		if($scope.isSpeaking){
+			recognition.stop();
+			$scope.isSpeaking = !$scope.isSpeaking;
+			console.log("when stop keyword", $scope.keyword);
+
+		}
+		else {
+			$scope.reset();
+			$scope.keyword = 0;
+         	text.value = "";
+			$scope.isSpeaking = !$scope.isSpeaking;
+			recognition.start();
+			console.log("when start keyword", $scope.keyword);
+		}
+	}	
+})
